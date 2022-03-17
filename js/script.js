1dc131cce94e4632f3885b6b97d7ade2706e79ae
big-pablo <fckem.nggaz@gmail.com>
2022-03-17 16:24:49 +0700

    Добавил генерацию таблицы
    todo:
    Проверка на то, что таблица уже сгенерирована
    Отцентрировать таблицу по середине страницы
    Естестна написать алгоритм построения лабиринта
    Желательно убрать расстояние между ячейками

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-result = prompt("Введи своё имя", "");
-alert("Добро пожаловать, " + result);
+console.log("Test");
+function generate(){
+    var body = document.getElementsByTagName("body")[0];
+    var size = document.getElementById("size").value;
+    var div = document.createElement("div");
+    console.log(size);
+    var tbl = document.createElement("table");
+    for (var j = 0; j < parseInt(size); j++) 
+    {
+        var row = document.createElement("tr");
+        for (var i = 0; i < parseInt(size); i++) 
+        {
+          var cell = document.createElement("td");
+          row.appendChild(cell);
+          var txt = document.createElement("p");
+          txt.innerText = "AAA||||";
+          cell.appendChild(txt);
+        }
+        tbl.appendChild(row);
+    }
+    div.appendChild(tbl)
+    body.appendChild(div);
+}
+  
